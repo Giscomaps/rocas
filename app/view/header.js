6c21c81ca824dce05245019f075e4f8a69910888
Giscomaps <63242716+Giscomaps@users.noreply.github.com>
2020-05-21 13:57:02 -0500

    Update header.js

--- app/view/header.js
+++ app/view/header.js
@@ -2,7 +2,7 @@ function Header(){
     return (
         <nav className='title'>
             <h1>Rock Type</h1>
-            <span className='ramaTwitter'><a href='https://twitter.com/ramgendeploy' target='_blank'>@ramgendeploy</a></span>
+            <span className='ramaTwitter'><a href='https://twitter.com/ramgendeploy' target='_blank'>jballes@unal.edu.co</a></span>
         </nav>
     )
 }
